f7cebf7a6429cd85c3754d693e87cbe9127aa67c
Kevin Miller <millerk22@math.ucla.edu>
2022-03-09 17:10:33 -0700

    added betavar

--- acquisitions.py
+++ acquisitions.py
@@ -2,6 +2,11 @@ import numpy as np
 from scipy.special import softmax
 
 
+def beta_var(A):
+    a0 = A.sum(axis=1)
+    a = (A * A).sum(axis=1)
+    return ((1. - a/(a0**2.))/(1. + a0))
+
 def unc(u):
     u_sort = np.sort(u)
     return 1. - (u_sort[:,-1] - u_sort[:,-2]) # smallest margin acquisition function
@@ -29,7 +34,6 @@ def uncsftmaxnorm(u):
 def uncnorm(u):
     return 1. - np.linalg.norm(u, axis=1)
 
-
 def vopt(u, C_a, evecs, gamma=0.1):
     Cavk = C_a @ evecs.T
     col_norms = np.linalg.norm(Cavk, axis=0)
@@ -49,7 +53,6 @@ def mcvopt(u, C_a, evecs, gamma=0.1):
     diag_terms = (gamma**2. + np.array([np.inner(evecs[k,:], Cavk[:, i]) for i,k in enumerate(np.arange(u.shape[0]))]))
     unc_terms = uncdist(u) # straightforward distance to current pseudolabel
     return unc_terms * col_norms **2. / diag_terms
-
 
 def random(u):
     return np.random.rand(u.shape[0])
