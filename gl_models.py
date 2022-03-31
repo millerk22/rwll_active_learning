b70a7faf3a87885cff6ab582607b01564066da62
Kevin Miller <millerk22@math.ucla.edu>
2022-03-31 09:37:39 -0600

    added decay RWLL model

--- gl_models.py
+++ gl_models.py
@@ -13,10 +13,14 @@ def get_models(G, model_names):
                'rwll001':poisson_rw_laplace(G, tau=0.001),
                'rwll01':poisson_rw_laplace(G, tau=0.01),
               'rwll1':poisson_rw_laplace(G, tau=0.1),
-             'beta0':beta_learning(G), 
+             'beta0':beta_learning(G),
                'beta001':beta_learning(G, tau=0.001),
                'beta01':beta_learning(G, tau=0.01),
-              'beta1':beta_learning(G, tau=0.1)}
+              'beta1':beta_learning(G, tau=0.1),
+              'rwlldecay01':poisson_rw_laplace_decay(G, tau=0.01, tau_ll=0.01),
+              'rwlldecayzero01': poisson_rw_laplace_decay(G, tau=0.0, tau_ll=0.01),
+              'rwlldecay1':poisson_rw_laplace_decay(G, tau=0.1, tau_ll=0.1),
+              'rwlldecayzero1': poisson_rw_laplace_decay(G, tau=0.0, tau_ll=0.1)}
 
     return [deepcopy(MODELS[name]) for name in model_names]
 
@@ -142,7 +146,7 @@ class poisson_rw_laplace(gl.ssl.ssl):
 
         return u
 
-    
+
 
 class beta_learning(gl.ssl.ssl):
     def __init__(self, W=None, class_priors=None, propagation='poisson', tau=0.0, eval_cutoff=50):
@@ -196,11 +200,11 @@ class beta_learning(gl.ssl.ssl):
             F = np.zeros((n, num_prop))
             F[prop_ind,:] = np.eye(num_prop)
             F -= np.mean(F, axis=0)
-            
+
             L = self.graph.laplacian()
             if self.tau  > 0.0:
                 L += self.tau*sparse.eye(L.shape[0])
-                
+
             P = gl.utils.conjgrad(L, F, tol=1e-5)
             P -= np.min(P, axis=0)
 
@@ -221,4 +225,118 @@ class beta_learning(gl.ssl.ssl):
             self.A = np.hstack((beta[:,np.newaxis], alpha[:,np.newaxis])) + 1. # include the Beta(1,1) prior
 
         u = self.A / (self.A.sum(axis=1)[:,np.newaxis]) # mean estimator
+        return u
+
+
+
+
+
+
+
+
+class poisson_rw_laplace_decay(gl.ssl.ssl):
+    def __init__(self, W=None, class_priors=None, tau=0.0, tau_ll=0.0, normalization='combinatorial', tol=1e-5, alpha=2, zeta=1e7, r=0.1):
+        """Laplace Learning
+        ===================
+
+        Semi-supervised learning via the solution of the Laplace equation
+        \\[L u_j = 0, \\ \\ j \\geq m+1,\\]
+        subject to \\(u_j = y_j\\) for \\(j=1,\\dots,m\\), where \\(L=D-W\\) is the
+        combinatorial graph Laplacian and \\(y_j\\) for \\(j=1,\\dots,m\\) are the
+        label vectors.
+
+        The original method was introduced in [1]. This class also implements reweighting
+        schemes `poisson` proposed in [2], `wnll` proposed in [3], and `properly`, proposed in [4].
+        If `properly` is selected, the user must additionally provide the data features `X`.
+
+        Parameters
+        ----------
+        W : numpy array, scipy sparse matrix, or graphlearning graph object (optional), default=None
+            Weight matrix representing the graph.
+        class_priors : numpy array (optional), default=None
+            Class priors (fraction of data belonging to each class). If provided, the predict function
+            will attempt to automatic balance the label predictions to predict the correct number of
+            nodes in each class.
+        tau : float (optional), default=0.0
+            Diagonal perturbation for controlling "model confidence"
+        tol : float (optional), default=1e-5
+            Tolerance for conjugate gradient solver.
+        alpha : float (optional), default=2
+            Parameter for `properly` reweighting.
+        zeta : float (optional), default=1e7
+            Parameter for `properly` reweighting.
+        r : float (optional), default=0.1
+            Radius for `properly` reweighting.
+
+        References
+        ---------
+        [1] X. Zhu, Z. Ghahramani, and J. D. Lafferty. [Semi-supervised learning using gaussian fields
+        and harmonic functions.](https://www.aaai.org/Papers/ICML/2003/ICML03-118.pdf) Proceedings
+        of the 20th International Conference on Machine Learning (ICML-03), 2003.
+
+        [2] J. Calder, B. Cook, M. Thorpe, D. Slepcev. [Poisson Learning: Graph Based Semi-Supervised
+        Learning at Very Low Label Rates.](http://proceedings.mlr.press/v119/calder20a.html),
+        Proceedings of the 37th International Conference on Machine Learning, PMLR 119:1306-1316, 2020.
+
+        [3] J. Calder, D. Slepčev. [Properly-weighted graph Laplacian for semi-supervised learning.](https://link.springer.com/article/10.1007/s00245-019-09637-3) Applied mathematics & optimization (2019): 1-49.
+        """
+        super().__init__(W, class_priors)
+
+        self.normalization = normalization
+        self.tau = tau       # tau for poisson reweighting
+        self.tau_ll = tau_ll # tau for LL solve
+        self.tol = tol
+
+        #Setup accuracy filename
+        fname = '_poisson_rw_laplace_decay'
+        self.name = 'Poisson Reweighted Laplace Learning with Decay'
+        if self.tau != 0.0:
+            fname += '_' + str(int(self.tau*1000)) + '_' + str(int(self.tau_ll*1000))
+        self.accuracy_filename = fname
+
+
+    def _fit(self, train_ind, train_labels, all_labels=None):
+
+        #Reweighting -- including tau
+        w = get_poisson_weighting(self.graph, train_ind, tau=self.tau) # tau not in the weighting, but in the solve after
+        D = sparse.spdiags(w, 0, w.size, w.size)
+        G = gl.graph(D * self.graph.weight_matrix * D)
+
+
+        #Get some attributes
+        n = G.num_nodes
+        unique_labels = np.unique(train_labels)
+        k = len(unique_labels)
+
+        #Graph Laplacian and one-hot labels
+        L = G.laplacian(normalization=self.normalization)
+        F = gl.utils.labels_to_onehot(train_labels)
+
+        #Locations of unlabeled points
+        idx = np.full((n,), True, dtype=bool)
+        idx[train_ind] = False
+
+        #Right hand side
+        b = -L[:,train_ind]*F
+        b = b[idx,:]
+
+        #Left hand side matrix
+        A = L[idx,:]
+        A = A[:,idx]
+        A += self.tau_ll*sparse.eye(L.shape[0]) # add the tau diagonal for Laplace Learning
+
+        #Preconditioner
+        m = A.shape[0]
+        M = A.diagonal()
+        M = sparse.spdiags(1/np.sqrt(M+1e-10),0,m,m).tocsr()
+
+        #Conjugate gradient solver
+        v = gl.utils.conjgrad(M*A*M, M*b, tol=self.tol)
+        v = M*v
+
+        #Add labels back into array
+        u = np.zeros((n,k))
+        u[idx,:] = v
+        u[train_ind,:] = F
+
         return u
