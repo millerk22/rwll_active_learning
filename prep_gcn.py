c90ee271d7c7a7df686a7f32fbbd6a86a84bde0e
Kevin Miller <millerk22@math.ucla.edu>
2022-05-17 08:42:24 -0600

    updated prep script

--- prep_gcn.py
+++ prep_gcn.py
@@ -1,4 +1,7 @@
 import torch
+import subprocess
+import sys
+
 
 def format_pytorch_version(version):
   return version.split('+')[0]
@@ -11,9 +14,14 @@ def format_cuda_version(version):
 
 CUDA_version = torch.version.cuda
 CUDA = format_cuda_version(CUDA_version)
+def install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package, "-f", f"https://pytorch-geometric.com/whl/torch-{TORCH}+{CUDA}.html"])
 
-!pip install torch-scatter     -f https://pytorch-geometric.com/whl/torch-{TORCH}+{CUDA}.html
-!pip install torch-sparse      -f https://pytorch-geometric.com/whl/torch-{TORCH}+{CUDA}.html
-!pip install torch-cluster     -f https://pytorch-geometric.com/whl/torch-{TORCH}+{CUDA}.html
-!pip install torch-spline-conv -f https://pytorch-geometric.com/whl/torch-{TORCH}+{CUDA}.html
-!pip install torch-geometric
+def install_basic(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
+install("torch-scatter")
+install("torch-sparse")
+install("torch-cluster")
+install("torch-spline-conv")
+install_basic("torch-geometric")
